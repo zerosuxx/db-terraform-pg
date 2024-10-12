c877f8b8296f6ff6f5c4f61dcc5d1a29adaaedbe
Tamas Mohos <zerosuxx@gmail.com>
2024-10-12 13:56:55 +0000

    add new grant

--- db-manager/main.tf
+++ db-manager/main.tf
@@ -1,7 +1,13 @@
 resource "postgresql_grant" "revoke_public" {
   database    = "postgres"
   role        = "public"
-  # schema      = "public"
   object_type = "database"
   privileges  = []
+}
+
+resource "postgresql_grant" "allow_app_ro" {
+  database    = "postgres"
+  role        = "app_ro"
+  object_type = "database"
+  privileges  = ["CONNECT"]
 }
